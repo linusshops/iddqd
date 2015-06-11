8190c52891e7de46480cc6a34e3ad5340cccdfe9
Dane MacMillan <work@danemacmillan.com>
2015-06-11 16:26:50 -0400

    Add helper method for checking URI strings.

--- src/app/code/community/Linus/Iddqd/Helper/Data.php
+++ src/app/code/community/Linus/Iddqd/Helper/Data.php
@@ -1,6 +1,26 @@
 <?php
 
+/**
+ * Class Linus_Iddqd_Helper_Data
+ *
+ * Provide some common verifications while in the observed event.
+ *
+ * @author Dane MacMillan <work@danemacmillan.com>
+ */
 class Linus_Iddqd_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
+    /**
+     * Check whether part of a URL is in the request URI.
+     *
+     * @param $urlPart
+     *
+     * @return bool
+     */
+    public function isInUrl($urlPart)
+    {
+        $requestUri = Mage::app()->getRequest()->getRequestUri();
+        return (stripos($requestUri, $urlPart) !== false)
+            ? true
+            : false;
+    }
 }
